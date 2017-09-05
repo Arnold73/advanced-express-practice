b1887e8f1326843979e767c6a6cd8309002c1a6a
Arnold73 <acpwells@gmail.com>
2017-09-04 21:56:13 -0500

    first 3 finished.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
 import comments from "./comments";
 import products from "./products";
 import vehicle from "./vehicles";
 import contacts from "./contacts";
 
 const app = express();
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
